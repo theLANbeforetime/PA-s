e1fc1d2a717ac557adfbf4996a0c2a746c66a71b
theLANbeforetime <traxxon.stecklein@gmail.com>
2018-09-09 19:20:07 -0600

    PA1 CSCI 1410
    First assigned project

---
+++ PA1_Stecklein.cpp
@@ -0,0 +1,80 @@
+/*
+Traxxon Stecklein
+CSCI 1410
+Programming Assignement 1
+Outline
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//print out myname & student ID
+
+//start loop 'choose option'
+  // print out 3 options
+
+    //option 1. almost always 99
+
+    //option 2. always 3
+
+    //option 3. exit
+
+//if choice not 1-3, then loop until choice is 1-3
+//end loop 'choice option' when valid choice is input -- continue to next step
+
+    //if option 1 and number less than 10, multiply number by 10
+
+      //break new number into a tens digit and ones digit
+
+      /*reverse or swap the digits places (ones place becomes 10s, vice versa)
+    and make it a new variable '2' -- print*/
+
+      //subtract var '2' by var '1' -- print
+
+      // if result is negative
+
+          //multiply by -1 --print
+
+      //else if result less than 10
+
+          //multiply by 10 -- print
+
+      //else do nothing
+
+    //take result and reverse digits again, make it a new variable '4' -- print
+
+      //add var '3' to var '4' and make new variable '5' -- print
+
+      // take var '5' and...
+
+        //if = 99 then print out congrats
+
+        //else print out something went wrong
+
+    //else if option 2
+
+      //prompt user to pick a number, make it a var 'userInput'
+
+      //double 'userInput' and make it new var 'num' -- print
+
+      //add 9 to 'num' -- print
+
+      //subtract 3 from 'num'  -- print
+
+      //divide 'num' by 2 -- print
+
+      //subtract 'num' by 'userInput'
+
+      //if answer = 3 print out congrats
+
+      //else print something went wrong
+
+    //else if option 3
+
+      //give user ending message and exit Program
+
+
+
+  return 0;
+}
