22e8383e9a37c44febcadd2eee5f82f3b697b7e4
theLANbeforetime <traxxon.stecklein@gmail.com>
2018-10-08 13:42:10 -0600

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,20 @@
+/*
+ * Traxxon Stecklein ID:
+ * Class: CSCI 1410
+ * PA 2 - Magic Eight Ball
+ * Status: Not running, just an outline
+ */
 
+ //include all relevant pre-processors
+
+ //create void function for option a, that can be called in main.cpp via functions.h
+
+ //create void function for option b, that can be called in main.cpp via functions.h
+
+ //create void function for option c, that can be called in main.cpp via functions.h
+
+ //create void function for option d, that can be called in main.cpp via functions.h
+
+ //create void function for option e, that can be called in main.cpp via functions.h
+
+ //create void function for option f, that can be called in main.cpp via functions.h
