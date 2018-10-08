9d7e5f2f5ab2d31a6e83f8322aa7fdb67c7e7201
theLANbeforetime <traxxon.stecklein@gmail.com>
2018-10-08 13:00:21 -0600

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,19 @@
 
+/*
+ * Traxxon Stecklein ID:
+ * Class: CSCI 1410
+ * PA 2 - Magic Eight Ball
+ * Status: Not running, just an outline
+ */
+
+//#ifndef UNTITLED_FUNCTIONS_H
+//#define UNTITLED_FUNCTIONS_H
+
+//set up definition for function option a: read responses from a file
+//set up definition for function option b: play magic eight ball
+//set up definition for function option c: sort by responses
+//set up definition for function option d: sort by category
+//set up definition for function option e: write responses to a file
+//set up definition for function option f: exit
+
+//#endif //UNTITLED_FUNCTIONS_H
